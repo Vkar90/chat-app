12918684ccbd6e52d3812ec7822679fd5c651d43
Vkar90 <kardamitsis.e@gmail.com>
2021-06-30 15:16:22 +0300

    Set up conversations and contacts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useLocalStorage from '../hooks/useLocalStorage';
 import  Login from './Login'
+import Dashboard from './Dashboard'
+import {ContactsProvider} from '../contexts/ContactsProvider'
+import {ConversationsProvider} from '../contexts/ConversationsProvider'
 
 function App() {
-  const [id, setId] = useState()
+  const [id, setId] = useLocalStorage()
+
+  const dashboard=(
+    <ContactsProvider>
+      <ConversationsProvider>
+        <Dashboard id={id} />
+        </ConversationsProvider>
+    </ContactsProvider>
+  )
+
   return (
-    <>
-      {id}
-      <Login onIdSubmit = {setId} />
-    </>
-  );
+      id ? <Dashboard id={id} /> :  <Login onIdSubmit = {setId} />
+  )
 }
 
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import { Container, Form, Button } from 'react-bootstrap'
+import { v4 as uuidV4} from 'uuid'
 
 export default function Login({onIdSubmit}) {
     const idRef = useRef()
@@ -10,6 +11,10 @@ export default function Login({onIdSubmit}) {
         onIdSubmit(idRef.current.value)
     }
 
+    function createNewId(){
+        onIdSubmit(uuidV4())
+    }
+
     return (
         <Container className="align-items-center d-flex" style = {{height: '100vh'}}>
             <Form onSubmit={handleSubmit}className="w-100">
@@ -17,8 +22,8 @@ export default function Login({onIdSubmit}) {
                     <Form.Label>Enter Your Id</Form.Label>
                     <Form.Control type='text' ref={idRef} required />
                 </Form.Group>
-                <Button type="submit" className="mr-2">Login</Button>
-                <Button variant="secondary">Create A New Id</Button>
+                <Button type="submit" className="me-2 mt-2">Login</Button>
+                <Button onClick={createNewId}variant="secondary" className="mt-2">Create A New Id</Button>
             </Form>
         </Container>
     )

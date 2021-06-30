dfd3efa0a27f3ad12cf43e4e7460b05db0091e49
Vkar90 <kardamitsis.e@gmail.com>
2021-06-30 18:27:16 +0300

    Finish client side

--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -20,7 +20,28 @@ const {contacts} = useContacts()
   }
 
   function addMessageToConversation({recipients, text, sender }) {
+      setConversations(prevConversations => {
+        let madeChange = false
+        const newMessage = { sender, text}
+        const newConversations = prevConversations.map(conversation => {
+          if (arrayEquality(conversation.recipients, recipients)){
+            madeChange = true
+            return {
+              ...conversation,
+              messages: [...conversation.messages, newMessage]
+            }
+          }
+          return conversation
+        })
 
+        if(madeChange){
+          return newConversations
+        }else{
+          return [
+            ...prevConversations, 
+            {recipients, messages: [newMessage]}]
+        }
+      })
   }
 
   function sendMessage(recipients, text) {
@@ -35,13 +56,25 @@ const {contacts} = useContacts()
           const name = (contact && contact.name) || recipient
           return { id: recipient, name }
       })
+
+      const messages = conversation.messages.map(message => {
+        const contact = contacts.find(contact => {
+          return contact.id === message.sender
+      })
+      const name = (contact && contact.name) || message.sender
+      const fromMe = id === message.sender
+      return {...message, senderName: name, fromMe}
+    })
+     
+
       const selected  = index === selectedConversationIndex
-      return { ...conversation, recipients, selected }
+      return { ...conversation, messages, recipients, selected }
   })
 
   const value = {
       conversations: formattedConversations, 
       selectedConversation: formattedConversations[selectedConversationIndex],
+      sendMessage,
       selectConversationIndex: setSelectedConversationIndex,
       createConversation
   }
@@ -51,4 +84,15 @@ const {contacts} = useContacts()
         {children}
     </ConversationsContext.Provider>
   )
+}
+
+function arrayEquality(a, b) {
+  if (a.length !== b.length)return false
+
+  a.sort()
+  b.sort()
+
+  return a.every((element, index) => {
+    return element === b[index]
+  })
 }
